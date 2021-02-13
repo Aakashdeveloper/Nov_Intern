1185892cc5009bab5f28e046936629fe12fe7760
aakashdeveloper <ahanda205@gmail.com>
2021-02-13 15:13:12 +0530

    added

--- day17,18,19,20-react/restaurentapp/src/component/Home/Home.js
+++ day17,18,19,20-react/restaurentapp/src/component/Home/Home.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Search from './Search';
 import QuickApi from './QuickApi';
 
-const Home = () => {
+const Home = (props) => {
+    console.log(props);
     return(
         <div>
             <Search/>
